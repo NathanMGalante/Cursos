7eb00c1acd8e30ee42517fe26c8daad647138e5e
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-06-22 10:00:06 -0300

    Publicando Endpoints - Usando DTO

--- Spring Boot API REST/forum/src/main/java/br/com/nathanmg/forum/controller/TopicosController.java
+++ Spring Boot API REST/forum/src/main/java/br/com/nathanmg/forum/controller/TopicosController.java
@@ -3,20 +3,19 @@ package br.com.nathanmg.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.nathanmg.forum.controller.dto.TopicoDto;
 import br.com.nathanmg.forum.modelo.Curso;
 import br.com.nathanmg.forum.modelo.Topico;
 
-@Controller
+@RestController	//com essa alteração, não precisa adicionar o ResponseBody
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> Lista(){
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+	public List<TopicoDto> Lista(){
+		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
