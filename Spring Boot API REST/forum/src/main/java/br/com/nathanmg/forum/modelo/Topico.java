d4543581cddf23d971bec661603ec86892f227a2
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-06-22 10:59:58 -0300

    Spring Data

--- Spring Boot API REST/forum/src/main/java/br/com/nathanmg/forum/modelo/Topico.java
+++ Spring Boot API REST/forum/src/main/java/br/com/nathanmg/forum/modelo/Topico.java
@@ -4,15 +4,34 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Topico {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String mensagem;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
-	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+	
+	@Enumerated(EnumType.STRING)//guarda como string.
+	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;//por padrão guarda na ordem.
+	
+	@ManyToOne
 	private Usuario autor;
+	
+	@ManyToOne
 	private Curso curso;
+	
+	@OneToMany(mappedBy = "topico")	//para não achar que é um novo mapeamento
 	private List<Resposta> respostas = new ArrayList<>();
 
 	public Topico(String titulo, String mensagem, Curso curso) {
