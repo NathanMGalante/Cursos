92cfe1046b224efe44cee1de04d269f27991a8a1
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-06-23 13:33:01 -0300

    Paginação e ordenação de recursos
    Ordenação

--- Spring Boot API REST/Seguranca, cache e monitoramento da API/forum/src/main/java/br/com/nathanmg/forum/controller/TopicosController.java
+++ Spring Boot API REST/Seguranca, cache e monitoramento da API/forum/src/main/java/br/com/nathanmg/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,10 +46,11 @@ public class TopicosController {
 	public Page<TopicoDto> Lista(
 	@RequestParam(required = false) String nomeCurso,
 	@RequestParam int pagina,
-	@RequestParam int paginaQuantidade){
+	@RequestParam int paginaQuantidade,
+	@RequestParam String ordenacao){
 		
 		//serve para se fazer paginação
-		Pageable paginacao = PageRequest.of(pagina, paginaQuantidade);
+		Pageable paginacao = PageRequest.of(pagina, paginaQuantidade, Direction.ASC, ordenacao);
 		
 		if(nomeCurso==null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
