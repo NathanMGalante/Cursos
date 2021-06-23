e5ca92fd10b154683af1a5692a14fa7b396898b7
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-06-23 14:54:09 -0300

    Proteção com Spring Security
    Liberando acesso aos endpoints públicos.

--- Spring Boot API REST/Seguranca, cache e monitoramento da API/forum/src/main/java/br/com/nathanmg/forum/config/security/SecurityConfigurations.java
+++ Spring Boot API REST/Seguranca, cache e monitoramento da API/forum/src/main/java/br/com/nathanmg/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.nathanmg.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+	//configurações de autentificação, como acesso, login,...
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+	
+	//configuração de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	//configurações de recursos estaticos, como js, css, imagens,...
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(web);
+	}
 }
