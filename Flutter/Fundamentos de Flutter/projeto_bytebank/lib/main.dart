7a4e153d644b2076350cc71c54ab77448cba94c7
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-07-12 14:02:54 -0300

    Montando o layout do formulário

--- Flutter/Fundamentos de Flutter/projeto_bytebank/lib/main.dart
+++ Flutter/Fundamentos de Flutter/projeto_bytebank/lib/main.dart
@@ -1,32 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        body: TransferList(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
-    ));
+void main() => runApp(ByteBankApp());
 
-class TransferList  extends StatelessWidget{
+class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransferItem(Transfer(111, 110.00)),
-        TransferItem(Transfer(222, 200)),
-        TransferItem(Transfer(333, 450)),
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
+      ),
     );
   }
 }
 
-class TransferItem  extends StatelessWidget{
+class TransferForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criar Transferência'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0,00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            child: Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          TransferItem(Transfer(111, 110.00)),
+          TransferItem(Transfer(222, 200)),
+          TransferItem(Transfer(333, 450)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
   Transfer _transfer;
+
   TransferItem(this._transfer);
 
   @override
