762404e310c8f94f6534952cae1540eb2eba7aa9
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-07-15 08:35:19 -0300

    Flutter: Configurando o builder do ListView

--- Flutter/Persistência de dados/projeto_bytebank/lib/screens/contacts/list.dart
+++ Flutter/Persistência de dados/projeto_bytebank/lib/screens/contacts/list.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_bytebank/models/contact.dart';
 import 'package:projeto_bytebank/screens/contacts/form.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = [];
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text('Contacts'),
         ),
-        body: ListView(
-          children: [
-            Card(
-              child: ListTile(
-                title: Text(
-                  'Nathan',
-                  style: TextStyle(fontSize: 24.0),
-                ),
-                subtitle: Text(
-                  '1000',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-            ),
-          ],
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            final Contact contact = contacts[index];
+            return _ContactItem(contact);
+          },
+          itemCount: contacts.length,
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => Navigator.of(context)
@@ -30,6 +24,26 @@ class ContactsList extends StatelessWidget {
                 (newContact) => debugPrint(newContact.toString()),
               ),
           child: Icon(Icons.add),
+        ),
+      );
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) => Card(
+        child: ListTile(
+          title: Text(
+            contact.name,
+            style: TextStyle(fontSize: 24.0),
+          ),
+          subtitle: Text(
+            contact.accountNumber.toString(),
+            style: TextStyle(fontSize: 16.0),
+          ),
         ),
       );
 }
