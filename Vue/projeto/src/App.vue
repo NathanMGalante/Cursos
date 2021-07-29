9a064843f692cdeeaa2f6552bee7a226443cc1b3
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2021-07-29 13:30:27 -0300

    Vue: Trabalhando com slots

--- Vue/projeto/src/App.vue
+++ Vue/projeto/src/App.vue
@@ -3,28 +3,32 @@
     <ul class="list">
       <li
         class="list-item"
-        v-for="foto of fotos"
+        v-for="foto in fotos"
       >
-        <div class="painel">
-          <h2 class="painel-title">{{ foto.titulo }}</h2>
-          <div class="painel-contents">
-            <img
-              :src="foto.url"
-              :alt="foto.title"
-            >
-          </div>
-        </div>
+        <painel
+          class="painel"
+          :title="foto.titulo"
+        >
+          <img
+            :src="foto.url"
+            :alt="foto.titulo"
+          >
+        </painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
 export default {
   data() {
     return {
       fotos: [],
     };
+  },
+  components: {
+    painel: Painel,
   },
   created() {
     this.$http
@@ -49,24 +53,6 @@ export default {
   }
 }
 .painel {
-  width: 200px;
-  height: 100%;
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  vertical-align: top;
-  text-align: center;
-
-  &-title {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    text-transform: uppercase;
-  }
-
   img {
     width: 100%;
   }
---
+++ Vue/projeto/src/components/shared/painel/Painel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-title">{{ title }}</h2>
+    <slot class="painel-contents">
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style lang="scss">
+.painel {
+  width: 200px;
+  height: 100%;
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  vertical-align: top;
+  text-align: center;
+
+  &-title {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    text-transform: uppercase;
+  }
+}
+</style>
